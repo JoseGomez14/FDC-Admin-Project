06393b1de8ba6e4939ab354326a9686c2ac68d72
JoseGomez14 <jose.gomez14@udea.edu.co>
2022-09-01 09:33:48 -0500

    Add React Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './components/main/Navbar';
+import Footer from './components/main/Footer';
+import  Main from './components/main/Main';
+import Login from './components/login/Login';
+import Create from './components/create/Create';
+import Edit from './components/edit/Edit';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={
+          <Main/>
+        }/>
+        <Route path='/login' element={
+          <Login/>
+        }/>
+        <Route path='/create' element={
+          <Create/>
+        }/>
+        <Route path='/edit/'>
+          <Route path=':id' element={
+            <Edit/>
+          }/>
+        </Route>
+      </Routes>
+      <Footer/>
+    </div>
   );
 }
 
